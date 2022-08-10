514072f11b82518d7e0191ce17c94bf85e0763e9
Behrooz Payoon <Behrooz.payoon@gmail.com>
2022-08-10 21:20:02 +0430

    Add project structure

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export APP_MODULE=${APP_MODULE-app.main:app}
+export HOST=${HOST:-0.0.0.0}
+export PORT=${PORT:-8000}  # 3
+export BACKEND_CORS_ORIGINS=${BACKEND_CORS_ORIGINS}  # 4
+
+
+# run gunicorn
+exec gunicorn --bind $HOST:$PORT "$APP_MODULE" -k uvicorn.workers.UvicornWorker  # 5
